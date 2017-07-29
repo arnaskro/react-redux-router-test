1e975175c60077b96480e0ff4e119ddd63eb897e
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-07-29 23:14:51 +0200

    some changes to the app.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store, { history } from './reducers/'
+import Users from './components/users/Users';
+import Programmers from './components/programmers/Programmers';
 import css from './App.scss';
 
 class App extends React.Component {
     render() {
         return (
-            <h1>It works!</h1>
+            <div className="container">
+                <br />
+                <div className="text-center">
+                    <h1>App is ready!</h1>
+                </div>
+            </div>
         );
     }
 }
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
